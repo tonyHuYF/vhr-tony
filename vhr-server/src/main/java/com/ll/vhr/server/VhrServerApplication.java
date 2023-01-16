16649db68939ee0b21dfc6a8d2b695e5dd0d13ee
huyf <724826175@qq.com>
2023-01-16 17:22:37 +0800

    项目初始化--配置所需要的依赖，设置

---
+++ vhr-server/src/main/java/com/ll/vhr/server/VhrServerApplication.java
@@ -0,0 +1,13 @@
+package com.ll.vhr.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrServerApplication.class, args);
+    }
+
+}
